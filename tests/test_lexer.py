6e60370046885079e34d84e73d5f3fdbc773178e
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-09-05 21:07:06 -0400

    Add the long test from the book, for later

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -3,7 +3,6 @@ import pytest
 
 from monkey import Lexer
 from monkey import Token
-from monkey import Token
 from monkey import token_types
 
 
@@ -12,18 +11,80 @@ class TestLexer:
         input_text = r"=;(){},+"
         lexer = Lexer(input_text)
 
-        assert lexer.next_token() == Token(token_types.ASSIGN, '=')
-        assert lexer.next_token() == Token(token_types.SEMICOLON, ';')
-        assert lexer.next_token() == Token(token_types.LPAREN, '(')
-        assert lexer.next_token() == Token(token_types.RPAREN, ')')
-        assert lexer.next_token() == Token(token_types.LBRACE, '{')
-        assert lexer.next_token() == Token(token_types.RBRACE, '}')
-        assert lexer.next_token() == Token(token_types.COMMA, ',')
-        assert lexer.next_token() == Token(token_types.PLUS, '+')
-        assert lexer.next_token() == Token(token_types.EOF, '' )
+        assert lexer.next_token() == Token(token_types.ASSIGN, "=")
+        assert lexer.next_token() == Token(token_types.SEMICOLON, ";")
+        assert lexer.next_token() == Token(token_types.LPAREN, "(")
+        assert lexer.next_token() == Token(token_types.RPAREN, ")")
+        assert lexer.next_token() == Token(token_types.LBRACE, "{")
+        assert lexer.next_token() == Token(token_types.RBRACE, "}")
+        assert lexer.next_token() == Token(token_types.COMMA, ",")
+        assert lexer.next_token() == Token(token_types.PLUS, "+")
+        assert lexer.next_token() == Token(token_types.EOF, "")
 
-    def test_raises_invalid_char(self):
+    @pytest.mark.skip
+    def test_sample_monkey_code(self):
+        monkey_code = "\n".join(
+            [
+                "let five = 5;               ",
+                "let ten = 10;               ",
+                "                            ",
+                "let add = fn(x, y) {        ",
+                "   x + y;                   ",
+                "};                          ",
+                "                            ",
+                "let result = add(five, ten);",
+            ]
+        )
+
+        lexer = Lexer(monkey_code)
+
+        expected = [
+            Token(token_types.LET, "let"),
+            Token(token_types.IDENTIFIER, "five"),
+            Token(token_types.ASSIGN, "="),
+            Token(token_types.INT, "5"),
+            Token(token_types.SEMICOLON, ";"),
+            Token(token_types.LET, "let"),
+            Token(token_types.IDENTIFIER, "ten"),
+            Token(token_types.ASSIGN, "="),
+            Token(token_types.INT, "10"),
+            Token(token_types.SEMICOLON, ";"),
+            Token(token_types.LET, "let"),
+            Token(token_types.IDENTIFIER, "add"),
+            Token(token_types.ASSIGN, "="),
+            Token(token_types.FUNCTION, "fn"),
+            Token(token_types.LPAREN, "("),
+            Token(token_types.IDENTIFIER, "x"),
+            Token(token_types.COMMA, ","),
+            Token(token_types.IDENTIFIER, "y"),
+            Token(token_types.RPAREN, ")"),
+            Token(token_types.LBRACE, "{"),
+            Token(token_types.IDENTIFIER, "x"),
+            Token(token_types.PLUS, "+"),
+            Token(token_types.IDENTIFIER, "y"),
+            Token(token_types.SEMICOLON, ";"),
+            Token(token_types.RBRACE, "}"),
+            Token(token_types.SEMICOLON, ";"),
+            Token(token_types.LET, "let"),
+            Token(token_types.IDENTIFIER, "result"),
+            Token(token_types.ASSIGN, "="),
+            Token(token_types.IDENTIFIER, "add"),
+            Token(token_types.LPAREN, "("),
+            Token(token_types.IDENTIFIER, "five"),
+            Token(token_types.COMMA, ","),
+            Token(token_types.IDENTIFIER, "ten"),
+            Token(token_types.RPAREN, ")"),
+            Token(token_types.SEMICOLON, ";"),
+            Token(token_types.EOF, ""),
+        ]
+
+        actual = [
+            lexer.next_token() for _ in range(len(expected))
+        ]
+
+        assert expected == actual
+
+    def test_illegal(self):
         input_text = r"~"
         lexer = Lexer(input_text)
-        with pytest.raises(ValueError):
-            lexer.next_token()
+        assert lexer.next_token().token_type == token_types.ILLEGAL
