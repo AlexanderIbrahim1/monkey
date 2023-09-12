b98c15e785093b74af9b048013bf86e02a5085e7
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-09-12 08:03:57 -0400

    Add constants for tokens with the same TokenType and Literal each time

---
+++ src/monkey/tokens/token_constants.py
@@ -0,0 +1,41 @@
+"""
+Some of the Token instances always have the same TokenType and Literal pair. To decrease
+the amount of boilerplate in the rest of the codebase, these Token instances are written
+as constants here.
+"""
+
+import monkey.tokens.token_types as token_types
+from monkey.tokens.monkey_token import Token
+
+# special token types
+EOF_TOKEN = Token(token_types.EOF, "")
+
+# operators
+ASSIGN_TOKEN = Token(token_types.ASSIGN, "=")
+PLUS_TOKEN = Token(token_types.PLUS, "+")
+MINUS_TOKEN = Token(token_types.MINUS, "-")
+BANG_TOKEN = Token(token_types.BANG, "!")
+ASTERISK_TOKEN = Token(token_types.ASTERISK, "*")
+SLASH_TOKEN = Token(token_types.SLASH, "/")
+LT_TOKEN = Token(token_types.LT, "<")
+GT_TOKEN = Token(token_types.GT, ">")
+EQ_TOKEN = Token(token_types.EQ, "==")
+NOT_EQ_TOKEN = Token(token_types.NOT_EQ, "!=")
+
+# punctuation
+COMMA_TOKEN = Token(token_types.COMMA, ",")
+SEMICOLON_TOKEN = Token(token_types.SEMICOLON, ";")
+
+LPAREN_TOKEN = Token(token_types.LPAREN, "(")
+RPAREN_TOKEN = Token(token_types.RPAREN, ")")
+LBRACE_TOKEN = Token(token_types.LBRACE, "{")
+RBRACE_TOKEN = Token(token_types.RBRACE, "}")
+
+# reserved keywords
+FUNCTION_TOKEN = Token(token_types.FUNCTION, "FUNCTION")
+LET_TOKEN = Token(token_types.LET, "LET")
+TRUE_TOKEN = Token(token_types.TRUE, "TRUE")
+FALSE_TOKEN = Token(token_types.FALSE, "FALSE")
+IF_TOKEN = Token(token_types.IF, "IF")
+ELSE_TOKEN = Token(token_types.ELSE, "ELSE")
+RETURN_TOKEN = Token(token_types.RETURN, "RETURN")
