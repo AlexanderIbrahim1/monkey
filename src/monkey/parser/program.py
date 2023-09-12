3b7a81b4809dd004e05697d321d507d9725a5877
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-09-12 08:07:10 -0400

    Make the Program type default-constructible

--- src/monkey/parser/program.py
+++ src/monkey/parser/program.py
@@ -3,6 +3,8 @@ This module contains the Program class, whose instances act as the root node of
 every AST that the parser produces.
 """
 
+from typing import Optional
+
 from monkey.parser.ast_node import ASTNode
 from monkey.parser.constants import DEFAULT_LITERAL
 from monkey.parser.statements import Statement
@@ -10,8 +12,11 @@ from monkey.tokens import Literal
 
 
 class Program(ASTNode):
-    def __init__(self, statements: list[Statement]) -> None:
-        self._statements = statements
+    def __init__(self, statements: Optional[list[Statement]] = None) -> None:
+        if statements is None:
+            self._statements: list[Statement] = list()
+        else:
+            self._statements = statements
 
     def token_literal(self) -> Literal:
         at_least_one_statement = len(self._statements) > 0
