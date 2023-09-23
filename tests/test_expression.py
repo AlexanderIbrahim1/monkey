79b680874d97081d9d356a3c46b998fe09cbe193
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-09-22 20:26:42 -0400

    Add test for printing out an if-expression

--- tests/test_expression.py
+++ tests/test_expression.py
@@ -1,8 +1,11 @@
 import pytest
 
 from monkey.parser.expressions import Expression
+from monkey.parser.expressions import IfExpression
 from monkey.parser.expressions import InfixExpression
 from monkey.parser.expressions import IntegerLiteral
+from monkey.parser.statements import BlockStatement
+from monkey.parser.statements import ExpressionStatement
 from monkey.tokens import Token
 from monkey.tokens import token_types
 
@@ -15,4 +18,38 @@ def test_infix_expression():
 
     infix = InfixExpression(token, left, operator, right)
 
-    assert str(infix) == "5 + 3"
+    assert str(infix) == "(5 + 3)"
+
+
+def test_if_expression():
+    token = Token(token_types.LBRACE, "{")
+    condition = InfixExpression(
+        Token(token_types.LT, "<"),
+        IntegerLiteral(Token(token_types.INT, "3"), "3"),
+        token_types.LT,
+        IntegerLiteral(Token(token_types.INT, "5"), "5"),
+    )
+
+    consequence = BlockStatement(
+        Token(token_types.LBRACE, "{"),
+        [
+            ExpressionStatement(
+                Token(token_types.INT, "10"),
+                IntegerLiteral(Token(token_types.INT, "10"), "10"),
+            )
+        ],
+    )
+
+    alternative = BlockStatement(
+        Token(token_types.LBRACE, "{"),
+        [
+            ExpressionStatement(
+                Token(token_types.INT, "20"),
+                IntegerLiteral(Token(token_types.INT, "20"), "20"),
+            )
+        ],
+    )
+
+    expr = IfExpression(token, condition, consequence, alternative)
+
+    assert str(expr) == "if (3 < 5) { 10 } else { 20 }"
