b76a17402fed640fbc3dce7659e5917f7991a533
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-09-23 20:29:51 -0400

    Add failed expressions and statements as global constants

--- src/monkey/parser/constants.py
+++ src/monkey/parser/constants.py
@@ -4,5 +4,10 @@ This module contains the constants that other parts of this project use.
 
 from monkey.tokens.token_types import Literal
 
+from monkey.parser.expressions import FailedExpression
+from monkey.parser.statements import FailedStatement
+
 
 DEFAULT_LITERAL: Literal = ""
+FAIL_EXPR = FailedExpression()
+FAIL_STMT = FailedStatement()
