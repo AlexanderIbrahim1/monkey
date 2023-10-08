716ec2c128b39b881b158fde0af4464059c7dc5d
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-10-08 17:55:03 -0400

    Make file agree with linters

--- src/monkey/parser/parser/_parse_string_literal.py
+++ src/monkey/parser/parser/_parse_string_literal.py
@@ -1,14 +1,10 @@
-import monkey.tokens.token_types as token_types
 import monkey.parser.expressions as exprs
 
-from monkey.parser.parser._constants import FAIL_EXPR
 from monkey.parser.parser._constants import ParsingFunction
 from monkey.parser.parser.parser import Parser
 
 
-def parse_string_literal(
-    parser: Parser, _: ParsingFunction
-) -> exprs.StringLiteral | exprs.FailedExpression:
+def parse_string_literal(parser: Parser, _: ParsingFunction) -> exprs.StringLiteral | exprs.FailedExpression:
     token = parser.current_token
     literal = parser.current_token.literal
 
