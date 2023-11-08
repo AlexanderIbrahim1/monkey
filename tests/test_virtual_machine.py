731d4286233ff3d82fc52e0d7c9abeda4704f868
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-11-07 20:23:13 -0500

    Sort tests into different functions for better organization

--- tests/test_virtual_machine.py
+++ tests/test_virtual_machine.py
@@ -26,13 +26,29 @@ class TestVirtualMachine:
         [
             VirtualMachineTestCase("0;", 0),
             VirtualMachineTestCase("1;", 1),
+            VirtualMachineTestCase("true;", True),
+            VirtualMachineTestCase("false;", False),
+        ],
+    )
+    def test_literal_expression_statement(self, test_case: VirtualMachineTestCase):
+        virtual_machine_test_case_internals(test_case)
+
+    @pytest.mark.parametrize(
+        "test_case",
+        [
             VirtualMachineTestCase("1 + 2;", 3),
             VirtualMachineTestCase("1 - 2;", -1),
             VirtualMachineTestCase("2 * 3;", 6),
             VirtualMachineTestCase("6 / 3;", 2),
             VirtualMachineTestCase("6 / 4;", 1),
-            VirtualMachineTestCase("true;", True),
-            VirtualMachineTestCase("false;", False),
+        ],
+    )
+    def test_arithmetic_infix_operator(self, test_case: VirtualMachineTestCase):
+        virtual_machine_test_case_internals(test_case)
+
+    @pytest.mark.parametrize(
+        "test_case",
+        [
             VirtualMachineTestCase("1 > 2;", False),
             VirtualMachineTestCase("2 < 1;", False),
             VirtualMachineTestCase("1 < 2;", True),
@@ -41,24 +57,51 @@ class TestVirtualMachine:
             VirtualMachineTestCase("1 != 1;", False),
             VirtualMachineTestCase("2 == 1;", False),
             VirtualMachineTestCase("2 != 1;", True),
+        ],
+    )
+    def test_integer_comparison_operator(self, test_case: VirtualMachineTestCase):
+        virtual_machine_test_case_internals(test_case)
+
+    @pytest.mark.parametrize(
+        "test_case",
+        [
             VirtualMachineTestCase("true == true;", True),
             VirtualMachineTestCase("false == false;", True),
             VirtualMachineTestCase("true == false;", False),
             VirtualMachineTestCase("true != true;", False),
             VirtualMachineTestCase("false != false;", False),
             VirtualMachineTestCase("true != false;", True),
+        ],
+    )
+    def test_boolean_literal_comparison_operator(self, test_case: VirtualMachineTestCase):
+        virtual_machine_test_case_internals(test_case)
+
+    @pytest.mark.parametrize(
+        "test_case",
+        [
             VirtualMachineTestCase("(1 < 2) == true;", True),
             VirtualMachineTestCase("(1 < 2) == false;", False),
             VirtualMachineTestCase("(1 > 2) == true;", False),
             VirtualMachineTestCase("(1 > 2) == false;", True),
+        ],
+    )
+    def test_infix_expression_equals_boolean_literal(self, test_case: VirtualMachineTestCase):
+        virtual_machine_test_case_internals(test_case)
+
+    @pytest.mark.parametrize(
+        "test_case",
+        [
             VirtualMachineTestCase("-5;", -5),
             VirtualMachineTestCase("-10;", -10),
             VirtualMachineTestCase("-0;", 0),
-            VirtualMachineTestCase("-10 + 20;", 10),
-            VirtualMachineTestCase("-10 + 20 - 10;", 0),
-            VirtualMachineTestCase("-(10 + 20);", -30),
-            VirtualMachineTestCase("-(10 + 20) + 25;", -5),
-            VirtualMachineTestCase("-(10 + 20) / 5 + 1;", -5),
+        ],
+    )
+    def test_minus_prefix_integer_operator(self, test_case: VirtualMachineTestCase):
+        virtual_machine_test_case_internals(test_case)
+
+    @pytest.mark.parametrize(
+        "test_case",
+        [
             VirtualMachineTestCase("!true;", False),
             VirtualMachineTestCase("!!true;", True),
             VirtualMachineTestCase("!false;", True),
@@ -66,6 +109,27 @@ class TestVirtualMachine:
             VirtualMachineTestCase("!123;", False),
             VirtualMachineTestCase("!!123;", True),
             VirtualMachineTestCase("!true == false;", True),
+        ],
+    )
+    def test_bang_prefix_boolean_operator(self, test_case: VirtualMachineTestCase):
+        virtual_machine_test_case_internals(test_case)
+
+    @pytest.mark.parametrize(
+        "test_case",
+        [
+            VirtualMachineTestCase("-10 + 20;", 10),
+            VirtualMachineTestCase("-10 + 20 - 10;", 0),
+            VirtualMachineTestCase("-(10 + 20);", -30),
+            VirtualMachineTestCase("-(10 + 20) + 25;", -5),
+            VirtualMachineTestCase("-(10 + 20) / 5 + 1;", -5),
+        ],
+    )
+    def test_minus_sign_with_arithmetic_operations(self, test_case: VirtualMachineTestCase):
+        virtual_machine_test_case_internals(test_case)
+
+    @pytest.mark.parametrize(
+        "test_case",
+        [
             VirtualMachineTestCase("if (true) { 10 };", 10),
             VirtualMachineTestCase("if (true) { 10 } else { 20 };", 10),
             VirtualMachineTestCase("if (false) { 10 } else { 20 };", 20),
@@ -76,20 +140,44 @@ class TestVirtualMachine:
             VirtualMachineTestCase("if (false) { 10 };", None),
             VirtualMachineTestCase("!(if (false) { 10 });", True),
             VirtualMachineTestCase("if ( (if (false) { 10 }) ) { 10 } else { 20 };", 20),
+        ],
+    )
+    def test_if_else_expression(self, test_case: VirtualMachineTestCase):
+        virtual_machine_test_case_internals(test_case)
+
+    @pytest.mark.parametrize(
+        "test_case",
+        [
             VirtualMachineTestCase("let one = 1; one;", 1),
             VirtualMachineTestCase("let one = 1; let two = 2; one + two;", 3),
             VirtualMachineTestCase("let one = 1; let two = one + one; one + two;", 3),
         ],
     )
-    def test_basic(self, test_case: VirtualMachineTestCase):
-        program = compiler_utils.parse(test_case.input_text)
-        compiler = comp.Compiler()
-        comp.compile(compiler, program)
+    def test_let_statement(self, test_case: VirtualMachineTestCase):
+        virtual_machine_test_case_internals(test_case)
 
-        bytecode = comp.bytecode_from_compiler(compiler)
-        machine = vm.VirtualMachine(bytecode)
-        vm.run(machine)
 
-        top_object = machine.stack.maybe_get_last_popped()
-        assert top_object is not None
-        assert object_utils.is_expected_object(top_object, test_case.expected)
+def virtual_machine_test_case_internals(test_case: VirtualMachineTestCase):
+    """
+    Assert that, after compilation and runnning by the VM, that the top object
+    on the stack is what you expect.
+
+    This method is so general for the test suite that all the tests end up using the
+    same body, with very little to no variation. As a result, it becomes difficult to
+    sort the different types of tests.
+
+    By extracting the body of the testing code into its own function, we can separate
+    the test cases into other functions with more descriptive names, allowing us to
+    be better organized.
+    """
+    program = compiler_utils.parse(test_case.input_text)
+    compiler = comp.Compiler()
+    comp.compile(compiler, program)
+
+    bytecode = comp.bytecode_from_compiler(compiler)
+    machine = vm.VirtualMachine(bytecode)
+    vm.run(machine)
+
+    top_object = machine.stack.maybe_get_last_popped()
+    assert top_object is not None
+    assert object_utils.is_expected_object(top_object, test_case.expected)
