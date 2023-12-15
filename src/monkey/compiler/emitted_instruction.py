31a756393b92c0617beb5422bc87f1d934d801fb
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-12-15 18:30:26 -0500

    Replace function with more general method in compiler class

--- src/monkey/compiler/emitted_instruction.py
+++ src/monkey/compiler/emitted_instruction.py
@@ -19,7 +19,3 @@ class EmittedInstruction:
 
 def is_valid_emitted_instruction(inst: EmittedInstruction) -> bool:
     return inst.opcode != opcodes.OPDUMMY or inst.position != DUMMY_EMITTED_INSTRUCTION_POSITION
-
-
-def is_pop(inst: EmittedInstruction) -> bool:
-    return inst.opcode == opcodes.OPPOP
