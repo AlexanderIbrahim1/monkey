2eb273b21274f1f26ddc9d87258f48be72033ce7
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-12-15 10:51:11 -0500

    Add more tests to clarify compilation scope behaviour

--- tests/test_compilation_scope.py
+++ tests/test_compilation_scope.py
@@ -37,6 +37,90 @@ class TestCompilerWithCompilationScopes:
         assert compiler.number_of_scopes == 2
         assert compiler.current_scope.instructions == Instructions()
 
+    def test_enter_scope_and_emit_instruction(self) -> None:
+        compiler = Compiler()
+
+        assert compiler.number_of_scopes == 1
+        assert compiler.current_scope.instructions == Instructions()
+
+        compiler.enter_scope()
+        compiler.emit(opcodes.OPMUL)
+
+        assert compiler.number_of_scopes == 2
+        assert compiler.current_scope.instructions == make_instruction(opcodes.OPMUL)
+
+    def test_emit_then_enter_scope_then_emit(self) -> None:
+        compiler = Compiler()
+
+        assert compiler.number_of_scopes == 1
+        assert compiler.current_scope.instructions == Instructions()
+
+        compiler.emit(opcodes.OPSUB)
+
+        assert compiler.number_of_scopes == 1
+        assert compiler.current_scope.instructions == make_instruction(opcodes.OPSUB)
+
+        compiler.enter_scope()
+
+        assert compiler.number_of_scopes == 2
+        assert compiler.current_scope.instructions == Instructions()
+
+        compiler.emit(opcodes.OPMUL)
+
+        assert compiler.number_of_scopes == 2
+        assert compiler.current_scope.instructions == make_instruction(opcodes.OPMUL)
+
+    def test_enter_and_leave_scope(self) -> None:
+        compiler = Compiler()
+        assert compiler.number_of_scopes == 1
+
+        compiler.enter_scope()
+        assert compiler.number_of_scopes == 2
+
+        compiler.leave_scope()
+        assert compiler.number_of_scopes == 1
+
+    def test_enter_two_scopes(self) -> None:
+        compiler = Compiler()
+        compiler.enter_scope()
+        compiler.enter_scope()
+
+        assert compiler.number_of_scopes == 3
+
+    def test_emit_after_entering_and_leaving_scopes(self) -> None:
+        opmul_instr = make_instruction(opcodes.OPMUL)
+        opadd_instr = make_instruction(opcodes.OPADD)
+        opsub_instr = make_instruction(opcodes.OPSUB)
+        opmulsub_instr = opmul_instr + opsub_instr
+
+        compiler = Compiler()
+
+        compiler.emit(opcodes.OPMUL)
+
+        assert compiler.number_of_scopes == 1
+        assert compiler.current_scope.instructions == opmul_instr
+        assert compiler.current_scope.last_instruction == EmittedInstruction(opcodes.OPMUL, 0)
+
+        compiler.enter_scope()
+        compiler.emit(opcodes.OPADD)
+
+        assert compiler.number_of_scopes == 2
+        assert compiler.current_scope.instructions == opadd_instr
+        assert compiler.current_scope.last_instruction == EmittedInstruction(opcodes.OPADD, 0)
+
+        compiler.leave_scope()
+
+        assert compiler.number_of_scopes == 1
+        assert compiler.current_scope.instructions == opmul_instr
+        assert compiler.current_scope.last_instruction == EmittedInstruction(opcodes.OPMUL, 0)
+
+        compiler.emit(opcodes.OPSUB)
+
+        assert compiler.number_of_scopes == 1
+        assert compiler.current_scope.instructions == opmulsub_instr
+        assert compiler.current_scope.second_last_instruction == EmittedInstruction(opcodes.OPMUL, 0)
+        assert compiler.current_scope.last_instruction == EmittedInstruction(opcodes.OPSUB, 1)
+
 
 class TestCompilationScope:
     def test_default_construction(self) -> None:
