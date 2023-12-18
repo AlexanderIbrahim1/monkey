1117f3831ed0fe764e249543d660de8079e9b7b5
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-12-17 19:38:42 -0500

    Add default factory for stack frames

--- src/monkey/virtual_machine/stack_frame.py
+++ src/monkey/virtual_machine/stack_frame.py
@@ -12,8 +12,22 @@ import monkey.object as objs
 @dataclasses.dataclass
 class StackFrame:
     function: objs.CompiledFunctionObject
+    _: dataclasses.KW_ONLY
+    base_pointer: int
     instruction_pointer: int = -1
 
     @property
     def instructions(self) -> code.Instructions:
         return self.function.instructions
+
+
+def default_stack_frame_factory() -> StackFrame:
+    # A factory function to create a default stack frame. This is separated from the
+    # definition of the class, because a default stack frame doesn't really make logical
+    # sense (what would a sensible default `CompiledFunctionObject` be?), and we should
+    # avoid having users accidentally construct one.
+
+    # However, we need a default stack frame when making space in the VM's frame stack,
+    # so we need some way of creating one
+    default_function = objs.CompiledFunctionObject(code.Instructions(), 0)
+    return StackFrame(default_function, base_pointer=0, instruction_pointer=0)
