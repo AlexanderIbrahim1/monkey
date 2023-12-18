2c125cdfe21760329b8d4676fc6171f68c69ff17
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-12-17 20:29:23 -0500

    Remove unneeded default factory

--- src/monkey/virtual_machine/stack_frame.py
+++ src/monkey/virtual_machine/stack_frame.py
@@ -19,15 +19,3 @@ class StackFrame:
     @property
     def instructions(self) -> code.Instructions:
         return self.function.instructions
-
-
-def default_stack_frame_factory() -> StackFrame:
-    # A factory function to create a default stack frame. This is separated from the
-    # definition of the class, because a default stack frame doesn't really make logical
-    # sense (what would a sensible default `CompiledFunctionObject` be?), and we should
-    # avoid having users accidentally construct one.
-
-    # However, we need a default stack frame when making space in the VM's frame stack,
-    # so we need some way of creating one
-    default_function = objs.CompiledFunctionObject(code.Instructions(), 0)
-    return StackFrame(default_function, base_pointer=0, instruction_pointer=0)
