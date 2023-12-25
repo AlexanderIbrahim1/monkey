a766ad6577ebd064e4330a2076dcdfc06f1667ad
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-12-25 18:23:58 -0500

    Make access to serializing functions easier

--- src/monkey/serialize/__init__.py
+++ src/monkey/serialize/__init__.py
@@ -2,3 +2,7 @@
 This subpackage contains code for serializing and deserializing the results of the compilation.
 It also contains code to visualize the serialized data.
 """
+
+from monkey.serialize.serialize import deserialize_bytecode
+from monkey.serialize.serialize import serialize_bytecode
+from monkey.serialize.serialize import SerializeError
